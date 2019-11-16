39ffb8f27da57408438de54747afd195aeefcdaa
Cluck <49716995+SMG233@users.noreply.github.com>
2019-11-16 15:06:54 +0800

    Update Component.lua

--- Component.lua
+++ Component.lua
@@ -1,9 +1,8 @@
-Component = function (name, t)
+-- Component
+
+function Component(name, all)
+    local t = all
     t.__id = name
-    return setmetatable(t, {
-        __call = function (self, entity)
-            entity.Component[self.__id] = setmetatable({}, self)
-            self.__index = self
-        end
-    })
+    t.Update, t.Draw = t.Update or function () end, t.Draw or function () end
+    return t
 end
